355918b51eb69d00262a1314eded6c73d926bc40
Tamas Biczo <biczo.tamas@gmail.com>
2018-01-21 11:36:08 +0100

    Create sliders with different behaviours

--- slider.js
+++ slider.js
@@ -1,6 +1,33 @@
-var app = new Vue({
-  el: "#app",
+var simpleSlider = new Vue({
+  el: "#simpleSlider",
   data: {
-    message: "Hello world!"
+    message: "Simple slider",
+    firstSliderValue: 50,
+    secondSliderValue: 50
+  },
+  watch: {
+    firstSliderValue: function(newVal, oldVal) {
+      var diff = newVal - oldVal;
+      this.secondSliderValue -= diff;
+    }
+  }
+})
+
+var doubleSlider = new Vue({
+  el: "#doubleSlider",
+  data: {
+    message: "Double moving slider",
+    firstSliderValue: 50,
+    secondSliderValue: 60  
+  },
+  watch: {
+    firstSliderValue: function(newVal, oldVal) {
+      var diff = 2 * (newVal - oldVal);
+      this.secondSliderValue -= diff;
+    }
+  },
+  mounted() {
+    this.$refs.firstSlider.max = this.secondSliderValue + (this.$refs.secondSlider.max - this.secondSliderValue) / 2;
+    this.$refs.firstSlider.min = this.secondSliderValue - (this.secondSliderValue - this.$refs.secondSlider.min) / 2;
   }
 })
